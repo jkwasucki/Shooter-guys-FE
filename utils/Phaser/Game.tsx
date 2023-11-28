7b16f3c96c4ce7446d94e7f27b353164f4c6177a
Jakub Kwasucki <jakubkwasucki@MacBook-Air-Jakub.local>
2023-11-28 17:48:52 +0100

    f

--- utils/Phaser/Game.tsx
+++ utils/Phaser/Game.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { useEffect } from 'react';
 import * as Phaser from 'phaser';
 import Main from './Scenes/Main';
@@ -6,30 +5,31 @@ import Main from './Scenes/Main';
 const PhaserGame = () => {
 
   useEffect(() => {
-    let game:Phaser.Game
-  
-    const config = {
-      type: Phaser.AUTO,
-      width: window.innerWidth,
-      height: window.innerHeight,
-      backgroundColor:"#4f303c",
-      physics: {
-        default: 'arcade',
-        arcade: {
-            debug: true
-        }
-    },
-      scene: [Main],
-    };
+    let game:Phaser.Game;
 
-    
-    game = new Phaser.Game(config);
-    game.canvas.style.cursor = 'url("/crosshair.png"), auto';
+    if (typeof window !== 'undefined') {
+      const config = {
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: "#4f303c",
+        physics: {
+          default: 'arcade',
+          arcade: {
+            debug: true
+          }
+        },
+        scene: [Main],
+      };
+
+      game = new Phaser.Game(config);
+      game.canvas.style.cursor = 'url("/crosshair.png"), auto';
+    }
 
     return () => {
-        if (game) {
-            game.destroy(true);
-        }
+      if (game) {
+        game.destroy(true);
+      }
     };
   }, []);
 
